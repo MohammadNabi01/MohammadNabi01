07f9e868a61b04e81aef675f68acaa5257caa6d0
Mohammad Nabi Gawhary <mohammadnabi.gawhar@learnet.se>
2023-11-27 20:38:34 +0100

    first commit

---
+++ Models/ContactForm.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace umbraco_course_submission.Models;
+
+public class ContactForm
+{
+    [Required(ErrorMessage = "Please enter your name")]
+    public string Name { get; set; } = null!;
+
+    [Required(ErrorMessage = "Please enter your email address")]
+    [EmailAddress]
+    public string Email { get; set; } = null!;
+
+    [Required(ErrorMessage = "Please enter your message")]
+    public string Message { get; set; } = null!;
+
+    public string? RedirectUrl { get; set; } = "/contacts";
+}
---
+++ Service/SubsService.cs
@@ -0,0 +1,25 @@
+﻿using umbraco_course_submission.Context;
+using umbraco_course_submission.Models;
+
+namespace umbraco_course_submission.Service
+{
+    public class SubService
+    {
+
+        private readonly DataContext _context;
+
+        public SubService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddSubsAsync(NewsLetterForm form)
+        {
+            if (_context.Subs != null)
+            {
+                _context.Subs.Add(new Entities.SubsEntity { Email = form.Email });
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
